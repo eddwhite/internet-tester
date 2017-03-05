ccd9aa4b8f5b8b4778a41e5f7dc143112933a0c3
eddwhite <eddwhite1@gmail.com>
2017-03-05 21:05:57 +0000

    Created script to import previous results into database. Tided up internet-tester slightly

--- internet-tester.py
+++ internet-tester.py
@@ -15,7 +15,7 @@ def main():
 	# open connection to the database
 	conn = db.connect(db_file, detect_types=db.PARSE_DECLTYPES|db.PARSE_COLNAMES)
 	# if table doesn't exist, create it
-	conn.execute('''CREATE TABLE IF NOT EXISTS speed (timestamp TIMESTAMP PRIMARY KEY, 
+	conn.execute('''CREATE TABLE IF NOT EXISTS results (timestamp TIMESTAMP PRIMARY KEY, 
 				 id INTEGER, sponsor TEXT, name TEXT, distance REAL, ping REAL, 
 				 download REAL, upload REAL)''')
 
@@ -25,15 +25,17 @@ def main():
 		# turn speedtest output into list
 		results = str(output)[2:-3].split(',')
 		assert(len(results) == 8)
-		# add entry to database
-		conn.execute("INSERT INTO results VALUES (?, ?, ?, ?, ?, ?, ?, ?)", results)
+		
 	else:
 		print("Internet down! Fuuuuuuuuu")
 		# get error number
 		errno = int(err_str.split('Errno ')[1].split(']')[0])
-		# add internet down entry to database
-		conn.execute("INSERT INTO results VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
-						[ts.now().isoformat(), errno] + [None for i in range(6)])
+		# create results
+		results = [ts.now().isoformat(), errno] + [None for i in range(6)]
+
+
+	# add results to database
+	conn.execute("INSERT INTO results VALUES (?, ?, ?, ?, ?, ?, ?, ?)", results)
 
 	# save changes to database and close
 	conn.commit()
