f3e676b8c47784e63cebd973cafa388588fad3d1
eddwhite <eddwhite1@gmail.com>
2017-03-09 11:52:41 +0000

    Full paths so it works with crontab. Reduced database lock time

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Check internet connection and speed and add results to a database
 Live website shows latest results and stats
 
-# Issues
+## Setup
+install speedtest-cli (github link HERE)
+crontab -e
+*/15 * * * * /usr/bin/python3 ~/Workspace/internet-tester/internet-tester.py > /dev/null
 
-# Todo
--- internet-tester.py
+++ internet-tester.py
@@ -4,20 +4,14 @@ import subprocess as sub
 import sqlite3 as db
 from datetime import datetime as ts
 
-db_file = 'speed.sqlite'
+db_file = "/home/edd/Workspace/internet-tester/speed.sqlite"
+speedtest_path = "/usr/local/bin/speedtest-cli"
 
 def main():
 	print("Running speedtest-cli...")
 	# run speedtest-cli and get results in csv format
-	p = sub.Popen(['speedtest-cli', '--csv'], stdout=sub.PIPE, stderr=sub.PIPE)
+	p = sub.Popen([speedtest_path, '--csv'], stdout=sub.PIPE, stderr=sub.PIPE)
 	output, errors = p.communicate()
-
-	# open connection to the database
-	conn = db.connect(db_file, detect_types=db.PARSE_DECLTYPES|db.PARSE_COLNAMES)
-	# if table doesn't exist, create it
-	conn.execute('''CREATE TABLE IF NOT EXISTS results (timestamp TIMESTAMP PRIMARY KEY, 
-				 id INTEGER, sponsor TEXT, name TEXT, distance REAL, ping REAL, 
-				 download REAL, upload REAL)''')
 
 	# if there are errors, internet is down
 	if errors == b'':
@@ -25,6 +19,8 @@ def main():
 		# turn speedtest output into list
 		results = str(output)[2:-3].split(',')
 		assert(len(results) == 8)
+		# rearrange output so timestamp is first
+		results = [results[3]] + [e for e in results[:3]] + [e for e in results[4:]]
 		
 	else:
 		print("Internet down! Fuuuuuuuuu")
@@ -34,7 +30,15 @@ def main():
 		results = [ts.now().isoformat(), errno] + [None for i in range(6)]
 
 
+	# open connection to the database
+        conn = db.connect(db_file, detect_types=db.PARSE_DECLTYPES|db.PARSE_COLNAMES)
+        # if table doesn't exist, create it
+        conn.execute('''CREATE TABLE IF NOT EXISTS results (timestamp TIMESTAMP PRIMARY KEY,
+                                 id INTEGER, sponsor TEXT, name TEXT, distance REAL, ping REAL,
+                                 download REAL, upload REAL)''')
+
 	# add results to database
+	print(results)
 	conn.execute("INSERT INTO results VALUES (?, ?, ?, ?, ?, ?, ?, ?)", results)
 
 	# save changes to database and close
